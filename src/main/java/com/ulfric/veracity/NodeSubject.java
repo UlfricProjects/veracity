2b159a0c2e08c59901dadaedae4d78ce9c7af1d9
Adam Harrison <ad@mharrison.us>
2017-09-01 06:25:39 -0400

    New XML assertions

--- src/main/java/com/ulfric/veracity/NodeSubject.java
+++ src/main/java/com/ulfric/veracity/NodeSubject.java
@@ -4,6 +4,8 @@ import org.w3c.dom.Node;
 
 import com.google.common.truth.FailureStrategy;
 
+import java.util.Objects;
+
 public final class NodeSubject extends VeracitySubject<NodeSubject, Node> implements NamedAssertions<Node> {
 
 	public NodeSubject(FailureStrategy failureStrategy, Node actual) {
@@ -13,6 +15,60 @@ public final class NodeSubject extends VeracitySubject<NodeSubject, Node> implem
 	@Override
 	public String getName() {
 		return actual().getNodeName();
+	}
+
+	public void hasNodeValue(String expected) {
+		if (expected == null) {
+			doesNotHaveNodeValue();
+			return;
+		}
+
+		hasNodeValue();
+
+		if (!actual().getNodeValue().equals(expected)) {
+			fail("has expected node value", expected);
+		}
+	}
+
+	public void hasNodeValue() {
+		isNotNull();
+
+		if (actual().getNodeValue() == null) {
+			fail("has node value");
+		}
+	}
+
+	public void doesNotHaveNodeValue() {
+		isNotNull();
+
+		if (actual().getNodeValue() == null) {
+			fail("has node value");
+		}
+	}
+
+	public void hasAttributeWithValue(String attribute, String expected) {
+		hasAttribute(attribute);
+
+		String actual = actual().getAttributes().getNamedItem(attribute).getNodeValue();
+		if (!Objects.equals(expected, actual)) {
+			fail("has attribute with expected", attribute, expected, actual);
+		}
+	}
+
+	public void hasAttribute(String attribute) {
+		hasAttributes();
+
+		if (actual().getAttributes().getNamedItem(attribute) == null) {
+			fail("has attribute", attribute);
+		}
+	}
+
+	public void hasAttributes() {
+		isNotNull();
+
+		if (!actual().hasAttributes()) {
+			fail("has attributes");
+		}
 	}
 
 }
